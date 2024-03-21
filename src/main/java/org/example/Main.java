26776340a735bc43f1998d92db3abbe1ba991826
Beibarys Learner <beibarys.learner@gmail.com>
2024-03-22 01:27:12 +0500

    500 error

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,10 +17,12 @@ public class Main {
     }
 
     @GetMapping("/greet")
-    public GreetResponse greet(){
-        // introduced new local varuablle
+    public GreetResponse greet(@RequestParam(value = "name", required = false) String name){
+        String greetMessage = name.isBlank() ? "Salem " : "Salem " + name;
+        // introduced new local variable
         GreetResponse response = new GreetResponse(
-                "Salem",
+//                "Salem",
+                greetMessage,
                 List.of("Java", "Python", "Javascript"),
                 new Person("James Gosling ", 68, 50_000_000.0)
         );
