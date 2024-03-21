0abc0d319bdc5e946fdf66b68fb38e5de1118d42
Beibarys Learner <beibarys.learner@gmail.com>
2024-03-22 00:09:27 +0500

    test commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        return new GreetResponse("Salem Java");
+    }
+
+    // add
+    record GreetResponse(String greet){}
+}
