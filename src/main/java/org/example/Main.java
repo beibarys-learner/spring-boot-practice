81bc7dd99b9a317cf5851af4f9e80dcbf501b2fb
Beibarys Learner <beibarys.learner@gmail.com>
2024-03-23 01:27:16 +0500

    updated customer Controller class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,8 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 @SpringBootApplication
-@RestController
-
 public class Main {
 
     //databases
@@ -18,26 +11,5 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
-//    @RequestMapping(path = "api/v1/customer" method = RequestMethod.GET) same as GetMapping
-    @GetMapping("api/v1/customers")
-    public List<Customer> getCustomers(){
-        return customers;
-    }
-
-
-    // write a second endpoint so we will return customer by ID
-    @GetMapping("api/v1/customers/{customerId}")
-    public Customer getCustomers(
-            @PathVariable("customerId") Integer customerId){
-        Customer customer = customers.stream().
-                filter(c -> c.id.equals(customerId))
-                .findFirst()
-                .orElseThrow(
-                        () -> new IllegalArgumentException("customer with id [$s] not found".formatted(customerId))
-                );
-        return customer;
-    }
-
 
 }
--- src/main/java/org/example/customer/CustomerController.java
+++ src/main/java/org/example/customer/CustomerController.java
@@ -1,4 +1,30 @@
 package org.example.customer;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CustomerController {
+    //    @RequestMapping(path = "api/v1/customer" method = RequestMethod.GET) same as GetMapping
+    @GetMapping("api/v1/customers")
+    public List<Customer> getCustomers(){
+        return customers;
+    }
+
+
+    // write a second endpoint so we will return customer by ID
+    @GetMapping("api/v1/customers/{customerId}")
+    public Customer getCustomers(
+            @PathVariable("customerId") Integer customerId){
+        Customer customer = customers.stream().
+                filter(c -> c.id.equals(customerId))
+                .findFirst()
+                .orElseThrow(
+                        () -> new IllegalArgumentException("customer with id [$s] not found".formatted(customerId))
+                );
+        return customer;
+    }
 }
