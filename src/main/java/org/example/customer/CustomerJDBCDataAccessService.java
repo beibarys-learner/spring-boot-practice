cf6086308d36900ac49d25e976a2d4a57935ea9a
Beibarys Learner <beibarys.learner@gmail.com>
2024-04-03 02:31:01 +0500

    updated row mapper

--- src/main/java/org/example/customer/CustomerJDBCDataAccessService.java
+++ src/main/java/org/example/customer/CustomerJDBCDataAccessService.java
@@ -13,9 +13,11 @@ import static net.sf.jsqlparser.parser.feature.Feature.update;
 public class CustomerJDBCDataAccessService implements CustomerDAO{
 
     private final JdbcTemplate jdbcTemplate;
+    private final CustomerRowMapper customerRowMapper;
 
-    public CustomerJDBCDataAccessService(JdbcTemplate jdbcTemplate) {
+    public CustomerJDBCDataAccessService(JdbcTemplate jdbcTemplate, CustomerRowMapper customerRowMapper) {
         this.jdbcTemplate = jdbcTemplate;
+        this.customerRowMapper = customerRowMapper;
     }
 
     @Override
@@ -25,18 +27,7 @@ public class CustomerJDBCDataAccessService implements CustomerDAO{
                 FROM customer
                 """;
 
-        RowMapper<Customer> customerRowMapper = (rs, rowNum) -> {
-            Customer customer = new Customer(
-                    rs.getLong("id"),
-                    rs.getString("name"),
-                    rs.getString("email"),
-                    rs.getInt("age")
-            );
-            return customer;
-        };
-
-        List<Customer> customers = jdbcTemplate.query(sql, customerRowMapper);
-        return  customers;
+        return jdbcTemplate.query(sql, customerRowMapper);
     }
 
 
