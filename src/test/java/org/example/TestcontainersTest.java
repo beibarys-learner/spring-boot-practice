0e34b9de895eceba22737593a7fb7efe6f9e7fcc
Beibarys Learner <beibarys.learner@gmail.com>
2024-04-10 23:26:50 +0500

    spring boot test excess beans

--- src/test/java/org/example/TestcontainersTest.java
+++ src/test/java/org/example/TestcontainersTest.java
@@ -2,7 +2,9 @@ package org.example;
 
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -13,7 +15,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
 @SpringBootTest
+
+// don't use for units this annotation
 public class TestcontainersTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Container
     private static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
             .withDatabaseName("code-dao-unit-test")
@@ -47,8 +55,11 @@ public class TestcontainersTest {
                 postgreSQLContainer.getUsername(),
                 postgreSQLContainer.getPassword()
         ).load();
-        flyway.migrate();
+//        flyway.migrate();
+        System.out.println(applicationContext.getBeanDefinitionCount());
+        for (String name : applicationContext.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
         System.out.println();
-
     }
 }
