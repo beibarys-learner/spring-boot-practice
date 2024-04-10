287a38582d89c36a5425de61ede0a5ec0fb4b3f8
Beibarys Learner <beibarys.learner@gmail.com>
2024-04-10 21:54:10 +0500

    add DynamicPropSource

--- src/test/java/org/example/TestcontainersTest.java
+++ src/test/java/org/example/TestcontainersTest.java
@@ -2,6 +2,9 @@ package org.example;
 
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -9,12 +12,27 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
-public class TestcontainerTest {
+@SpringBootTest
+public class TestcontainersTest {
     @Container
     private static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
             .withDatabaseName("code-dao-unit-test")
             .withUsername("code")
             .withPassword("password");
+
+    @DynamicPropertySource
+    private static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add(
+                "spring.datasource.url", postgreSQLContainer::getJdbcUrl
+        );
+        // allows to register properties
+        registry.add(
+                "spring.datasource.username", postgreSQLContainer::getUsername
+        );
+        registry.add(
+                "spring.datasource.password", postgreSQLContainer::getPassword
+        );
+    }
 
     @Test
     void canStartPostgresDB() {
